293edbafbf9266486ae11473d9d801f2e9a1bd04
edsor <edsor1995@gmail.com>
2023-03-14 19:29:48 +0300

    1 com

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,5 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Lesson1 {
     public static void main(String[] args) {
+        int []ggr  = new int[10];
+        ggr[0] = 1;
+        ggr[2] = 1;
 
+        System.out.println(ggr.length);
+        System.out.println(Arrays.toString(ggr));
     }
 }
